cb4c0b34e0245c6650f36125e0222f5bcea85a37
changsj <sijie.chang@gmail.com>
2015-02-14 22:10:58 +0800

    update def create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+  
   def index
     @articles = Article.all
   end
@@ -9,7 +11,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
   end
