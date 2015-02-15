b54039c10ccf9842137704cddb02582b2b8512fe
changsj <sijie.chang@gmail.com>
2015-02-15 20:19:17 +0800

    tag in webpage with resources and TagsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
+  root 'articles#index'
   resources :articles do
     resources :comments
   end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'articles#index'
+  resources :tags
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
