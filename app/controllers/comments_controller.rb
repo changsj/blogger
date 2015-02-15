c692fe52901b2dd5680cfb91051894eb900333f0
changsj <sijie.chang@gmail.com>
2015-02-15 15:11:51 +0800

    standardize with comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
   include CommentsHelper
-  
-  def create
-    @comment = Comment.new(comment_params)
-    @comment.article_id = params[:article_id]
 
-    @comment.save
-    redirect_to article_path(@comment.article)
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    
+    # @comment = Comment.new(comment_params)
+    # @comment.article_id = params[:article_id]
+
+    # @comment.save
+
+    redirect_to article_path(@article)
   end
 end
